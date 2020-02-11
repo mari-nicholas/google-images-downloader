39945191576d200438cb1c10c029c2e2afd2887d
Joshua Guinness <joshua.guinness@gmail.com>
2020-02-11 11:01:55 -0500

    Cleaned up uneeded code. Added command line colour formatting

--- src/POC/keyword-download.py
+++ src/POC/keyword-download.py
@@ -6,14 +6,13 @@ from urllib.request import Request, urlopen
 
 def user_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument("keyword", help="keywords of google images you want")
+    parser.add_argument("keyword", help="keyword for the google images you want")
     args = parser.parse_args()
     return args.keyword
 
 def construct_url(keyword):
 
     params = '&tbm='
-
     url = 'https://www.google.com/search?q=' + quote(
                 keyword.encode('utf-8')) + '&espv=2&sxsrf=ACYBGNSwqBUElVjmEWOTu3-mXPnReqFoLw:1581376760401&source=lnms' + params + 'isch&sa=X&ved=2ahUKEwiY7bzAj8jnAhUQjq0KHbXwBEYQ_AUoAXoECBMQAw&biw=838&bih=880'
 
@@ -27,10 +26,9 @@ def get_html(url):
         req = urllib.request.Request(url, headers=headers)
         response = urllib.request.urlopen(req)
         data = str(response.read())
-        #print("URL Read\n")
         return data
     except Exception as e:
-        print("Could not open URL.")
+        print("Could not get page URL")
         sys.exit()
 
 def get_image_url(raw_html):
@@ -40,22 +38,22 @@ def get_image_url(raw_html):
         link = "no_links"
         return link, end   
     else:
-        type_of_image = get_image_type(start, raw_html)
+        # type_of_image = get_image_type(start, raw_html)
         #These lines isolate the link in the raw_html
         beg_link = raw_html.find('data-iurl="', start+1)
         end_link = raw_html.find('jsname="', beg_link+1)
         link = str(raw_html[beg_link+11:end_link-2])
-        return link, end_link, type_of_image
+        return link, end_link
 
-def get_image_type(start, raw_html):
-    try:
-        beg_type = raw_html.find('src="data:image/', start)
-        end_type = raw_html.find(';', beg_type+1)
-        type_of_image = str(raw_html[beg_type+16:end_type])
-        return type_of_image
-    except Exception as e:
-        print("Could not find image type")
-        sys.exit()
+# def get_image_type(start, raw_html):
+#     try:
+#         beg_type = raw_html.find('src="data:image/', start)
+#         end_type = raw_html.find(';', beg_type+1)
+#         type_of_image = str(raw_html[beg_type+16:end_type])
+#         return type_of_image
+#     except Exception as e:
+#         print("Could not find image type")
+#         sys.exit()
 
 def write_to_file(image_url, keyword, i):
      try:
@@ -64,29 +62,26 @@ def write_to_file(image_url, keyword, i):
          data = response.read()
          response.close()
          path = "Images/" + keyword + str(i) + ".jpeg" 
-         print(path)
          output_file = open(path, 'wb')
          output_file.write(data)
          output_file.close()
      except Exception as e:
-         print("Something Went Wrong...")
+         print("Something went wrong when downloading and saving image")
 
 
 def download_images(raw_html, keyword):
-    limit = 1
-    i = 0
+    limit = 5
     sliced_html = raw_html
     for i in range(0, limit):
         #Gets Links
-        image_url, cutoff, type_of_image = get_image_url(sliced_html)
+        image_url, cutoff = get_image_url(sliced_html)
         #No links => End Program 
         if(image_url == "no_links"):
             break
         else:
-            print(type_of_image)
-            print(image_url, "\n")
+            print("Getting thumbnail from:\t", image_url)
             write_to_file(image_url, keyword, i)
-            print("Image", (i+1), "Downloaded...\n")
+            print('\033[0;32m' + "Image", (i+1), "downloaded\n" + '\033[0m')
             sliced_html = sliced_html[cutoff:]
 
 
@@ -96,9 +91,10 @@ def main():
     url = construct_url(keyword)
     raw_html = get_html(url)
 
-    print("Downloading Images...")
+    print('\033[1;33m' + "\nDownloading Images...\n" + '\033[0m')
     download_images(raw_html, keyword)
-    print("Download Complete")
+    print('\033[1;32m' + "Download Complete\n" + '\033[0m')
 
 if __name__ == '__main__':
     main()
+    
