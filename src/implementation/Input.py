d3d131425b4b9de1b28fcdf650483dfab28f14c0
unknown <marin@mcmaster.ca>
2020-03-16 22:22:44 -0400

    Setup basic Input.py

--- src/implementation/Input.py
+++ src/implementation/Input.py
@@ -1,8 +1,28 @@
 # Secrets: The format and structure of the input data.
 # Services: Converts  the  input  data  into  the  data  structure  used  by  the  input  parameters module.
 
-def UserInput():
-    print("User Input Function")
+import argparse
 
-def keywordFromFile():
-    print("Keyword From File Function")
+def UserInput():
+	fileCheck = argparse.ArgumentParser()
+	fileCheck.add_argument('-f', '--file', default='', type=str, required=False, help='gets args from file. Takes file path to .txt file')
+
+	fileArg = fileCheck.parse_args()
+
+	if(fileArg.file):
+		return keywordFromFile(fileArg.file)
+	else:
+		parser = argparse.ArgumentParser()
+		parser.add_argument('-k', '--keyword', required=True , help='Keyword for the google images you want')
+		parser.add_argument('-l', '--limit', help='Limit for the number of images you want')
+
+		args = fileCheck.parse_args()
+		print('User Input Function')
+
+
+def keywordFromFile(filePath):
+    print('Keyword From File Function')
+
+
+if __name__ == '__main__':
+	UserInput()
