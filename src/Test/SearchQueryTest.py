2e1eac973eb6badde4381d7400443f1cd9d04768
unknown <marin@mcmaster.ca>
2020-03-29 20:36:55 -0400

    added more unit tests for sq module

--- src/Test/SearchQueryTest.py
+++ src/Test/SearchQueryTest.py
@@ -3,7 +3,7 @@
 #  @brief Tests the functionality of SearchQuery.py
 #  @date 03/28/2020
 
-from os import path
+from os import curdir, path
 import sys
 
 from inspect import currentframe, getfile
@@ -20,13 +20,20 @@ class TestBuildURLParam:
 	def test_no_params(self):
 
 		args = {
+					"keyword" : "default",
+					"limit" : 10,
+					"safesearch" : False,
+					"directory" : path.join(curdir, "Images"),
 					"filetype" : "",
 					"colour" : "",
 					"license" : "",
 					"imagetype" : "",
 					"imageage" : "",
 					"aspectratio" : "",
-					"imagesize" : ""
+					"imagesize" : "",
+					"serverhost" : "",
+					"serverusername" : "",
+					"serverpassword" : ""
 				}
 
 		params = buildURLParam(args)
@@ -37,15 +44,74 @@ class TestBuildURLParam:
 
 	def test_all_params(self):
 		args = {
+					"keyword" : "default",
+					"limit" : 10,
+					"safesearch" : False,
+					"directory" : path.join(curdir, "Images"),
 					"filetype" : "jpg",
 					"colour" : "red",
 					"license" : "labeled-for-noncommercial-reuse-with-modification",
 					"imagetype" : "photo",
 					"imageage" : "past-7-days",
 					"aspectratio" : "wide",
-					"imagesize" : "medium"
+					"imagesize" : "medium",
+					"serverhost" : "",
+					"serverusername" : "",
+					"serverpassword" : ""
 				}
 
 		params = buildURLParam(args)
 
 		assert params == 'iar:w,ic:specific,isc:red,ift:jpg,qdr:w,isz:m,itp:photo,sur:fm'
+
+class TestBuildURL:
+
+	def test_no_params(self):
+		args = {
+					"keyword" : "default",
+					"limit" : 10,
+					"safesearch" : False,
+					"filetype" : "",
+					"directory" : "",
+					"colour" : "",
+					"colourtype" : "",
+					"license" : "",
+					"imagetype" : "",
+					"imageage" : "",
+					"aspectratio" : "",
+					"imagesize" : "",
+					"serverhost" : "",
+					"serverusername" : "",
+					"serverpassword" : ""
+				}
+
+		url = buildURL(args)
+
+		assert url == 'https://www.google.com/search?q=default' + \
+        '&tbm=isch&hl=en&hl=en&tbs='+ \
+        '&ved=0CAMQ2J8EahcKEwjgteiDqrToAhUAAAAAHQAAAAAQAg&biw=1519&bih=760'
+
+
+	def test_all_params(self):
+		args = {
+					"keyword" : "default",
+					"limit" : 10,
+					"safesearch" : True,
+					"directory" : path.join(curdir, "Images"),
+					"filetype" : "jpg",
+					"colour" : "red",
+					"license" : "labeled-for-noncommercial-reuse-with-modification",
+					"imagetype" : "photo",
+					"imageage" : "past-7-days",
+					"aspectratio" : "wide",
+					"imagesize" : "medium",
+					"serverhost" : "",
+					"serverusername" : "",
+					"serverpassword" : ""
+				}
+
+		url = buildURL(args)
+
+		assert url == 'https://www.google.com/search?q=' + 'default' + \
+        '&tbm=isch&hl=en&hl=en&tbs=' + quote('iar:w,ic:specific,isc:red,ift:jpg,qdr:w,isz:m,itp:photo,sur:fm'.encode('utf-8')) + \
+        '&ved=0CAMQ2J8EahcKEwjgteiDqrToAhUAAAAAHQAAAAAQAg&biw=1519&bih=760&safe=active'
