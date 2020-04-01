754dc4a811515b3830652c1005f72b5b3a56440f
unknown <marin@mcmaster.ca>
2020-03-31 22:09:43 -0400

    cleaned up Input and added some extra error handling

--- src/Input.py
+++ src/Input.py
@@ -31,20 +31,20 @@ def userInput():
 		cmdParser.add_argument('-k', '--keyword', required=True, help='keyword for the Google images you want')
 		cmdParser.add_argument('-l', '--limit', type=int, default=10, help='limit for the number of images you want')
 		cmdParser.add_argument('-ss', '--safesearch', default=False, help='turns on safe search', action='store_true')
-		cmdParser.add_argument('-ft', '--filetype', type=str, required=False, default='', help='download images of specific file type', choices=['jpg', 'gif', 'png', 'bmp', 'svg', 'webp', 'ico', 'raw'])
-		cmdParser.add_argument('-d', '--directory', type=str, required=False, default=path.join(curdir, 'Images'), help='download images to folder in a specific directory')
-		cmdParser.add_argument('-c', '--colour', type=str, required=False, default='', help='search for images with specific colours', choices=['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'purple', 'pink', 'white', 'gray', 'black', 'brown', 'full-color', 'black-and-white', 'transparent'])
-		cmdParser.add_argument('-li', '--license', type=str, required=False, default='', help='search based on what image is licensed for', choices=['labeled-for-reuse-with-modifications','labeled-for-reuse','labeled-for-noncommercial-reuse-with-modification','labeled-for-nocommercial-reuse'])
-		cmdParser.add_argument('-t', '--imagetype',  type=str, required=False, default='', help='search for a specific type of image', choices=['face','photo','clipart','line-drawing','animated'])
-		cmdParser.add_argument('-a', '--imageage', type=str, required=False, default='', help='search for how long ago the image was uploaded', choices=['past-24-hours','past-7-days','past-month','past-year'])
-		cmdParser.add_argument('-ar', '--aspectratio', type=str, required=False, default='', help='search based on the aspect ratio of the image', choices=['tall', 'square', 'wide', 'panoramic'])
-		cmdParser.add_argument('-is', '--imagesize', type=str, required=False, default='', help='search based on approximate size of image desired', choices=['large','medium','icon','>400*300','>640*480','>800*600','>1024*768','>2MP','>4MP','>6MP','>8MP','>10MP','>12MP','>15MP','>20MP','>40MP','>70MP'])
-		cmdParser.add_argument('-s', '--serverhost', type=str, required=False, default='', help='specify a server hostname to download the images to')
-		cmdParser.add_argument('-u', '--serverusername', type=str, required=False, default='', help='specify a server username')
-		cmdParser.add_argument('-p', '--serverpassword', type=str, required=False, default='', help='specify a server password')
+		cmdParser.add_argument('-ft', '--filetype', type=str, help='download images of specific file type', choices=['jpg', 'gif', 'png', 'bmp', 'svg', 'webp', 'ico', 'raw'])
+		cmdParser.add_argument('-d', '--directory', type=str, default=path.join(curdir, 'Images'), help='download images to folder in a specific directory')
+		cmdParser.add_argument('-c', '--colour', type=str, help='search for images with specific colours', choices=['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'purple', 'pink', 'white', 'gray', 'black', 'brown', 'full-color', 'black-and-white', 'transparent'])
+		cmdParser.add_argument('-li', '--license', type=str, help='search based on what image is licensed for', choices=['labeled-for-reuse-with-modifications','labeled-for-reuse','labeled-for-noncommercial-reuse-with-modification','labeled-for-nocommercial-reuse'])
+		cmdParser.add_argument('-t', '--imagetype',  type=str, help='search for a specific type of image', choices=['face','photo','clipart','line-drawing','animated'])
+		cmdParser.add_argument('-a', '--imageage', type=str, help='search for how long ago the image was uploaded', choices=['past-24-hours','past-7-days','past-month','past-year'])
+		cmdParser.add_argument('-ar', '--aspectratio', type=str, help='search based on the aspect ratio of the image', choices=['tall', 'square', 'wide', 'panoramic'])
+		cmdParser.add_argument('-is', '--imagesize', type=str, help='search based on approximate size of image desired', choices=['large','medium','icon','>400*300','>640*480','>800*600','>1024*768','>2MP','>4MP','>6MP','>8MP','>10MP','>12MP','>15MP','>20MP','>40MP','>70MP'])
+		cmdParser.add_argument('-s', '--serverhost', type=str, help='specify a server hostname to download the images to')
+		cmdParser.add_argument('-u', '--serverusername', type=str, help='specify a server username')
+		cmdParser.add_argument('-p', '--serverpassword', type=str, help='specify a server password')
 		# cmdParser.add_argument('-rp', '--remotepath', type=str, required=False, default='', help='specify a remote path to save the images to')
-		cmdParser.add_argument('-wl', '--whitelist', type=str, required=False, default='', help='only get images from a specified website or domain')
-		cmdParser.add_argument('-rg', '--region', type=str, required=False, default='', help='get images uploaded in a specific region', 
+		cmdParser.add_argument('-wl', '--whitelist', type=str, help='only get images from a specified website or domain')
+		cmdParser.add_argument('-rg', '--region', type=str, help='get images uploaded in a specific region', 
 			choices=['Afghanistan', 'Albania', 'Algeria', 'American Samoa', 'Andorra', 'Angola', 'Anguilla', 'Antigua & Barbuda', 'Argentina', 'Armenia', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas', 'Bahrain', 'Bangladesh', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bhutan', 'Bolivia', 'Bosnia & Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Central African Republic', 'Chad', 'Chile', 'Colombia', 'Congo - Brazzaville', 'Congo - Kinshasa', 'Cook Islands', 'Costa Rica', 'Côte d’Ivoire', 'Croatia', 'Cuba', 'Cyprus', 'Czechia', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana', 'Gibraltar', 'Greece', 'Greenland', 'Guatemala', 'Guernsey', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritius', 'Mexico', 'Micronesia', 'Moldova', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Myanmar (Burma)', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Niue', 'North Macedonia', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Pitcairn Islands', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Romania', 'Russia', 'Rwanda', 'Samoa', 'San Marino', 'São Tomé & Príncipe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St. Helena', 'St. Vincent & Grenadines', 'Suriname', 'Sweden', 'Switzerland', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor-Leste', 'Togo', 'Tonga', 'Trinidad & Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'U.S. Virgin Islands', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Zambia', 'Zimbabwe'])
 		cmdParser.add_argument('-bl', '--blacklist', type=str, help='Stop program from downloading images from specified websites')
 		args = cmdParser.parse_args()
@@ -62,7 +62,12 @@ def userInput():
 def keywordFromFile(filePath):
 	#print('Keyword From File Function')
 	args = {}
-	f = open(filePath)
+	try:
+		f = open(filePath)
+	except:
+		raise Exception("Could not find the file you specified." + \
+		 				"If you are on Windows, make sure your file path uses double backslashes (\\\\) rather than single backslashes (\\)")
+	
 	args = load(f)
 
 	if "keyword" not in args:
@@ -78,4 +83,5 @@ def keywordFromFile(filePath):
 	if "safesearch" not in args: args["safesearch"] = False
 	if "directory" not in args: args["directory"] = path.join(curdir, 'Images')
 
+	f.close()
 	return args
