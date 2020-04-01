6a334ea71f9a58cfbe01ac0bda57b1916f8c6c86
unknown <marin@mcmaster.ca>
2020-04-01 13:43:08 -0400

    finished tests for command line inputs

--- src/Test/InputTest.py
+++ src/Test/InputTest.py
@@ -15,6 +15,81 @@ sys.path.insert(0, parent_dir)
 
 from Input import *
 
+class TestUserInput:
+
+	def test_nothing_but_keyword(self):
+		sys.argv = ['Main.py', '-k', 'test']
+		args = userInput()
+
+		assert args == {
+							"keyword" : "test",
+							"limit" : 10,
+							"safesearch" : False,
+							"directory" : ".\\Images",
+							"filetype" : "",
+							"colour" : "",
+							"license" : "",
+							"imageage" : "",
+							"imagetype" : "",
+							"aspectratio" : "",
+							"imagesize" : "",
+							"serverhost" : "",
+							"serverusername" : "",
+							"serverpassword" : "",
+							"whitelist": "",
+							"region" : "",
+							"blacklist": ""
+						}
+
+	def test_from_file(self):
+		sys.argv = ['Main.py', '-ff', '.\\ConfigForTesting\\allParams.txt']
+
+		args = userInput()
+
+		assert args == {
+							"keyword" : "Default",
+							"limit" : 10,
+							"safesearch" : True,
+							"directory" : ".\\Images",
+							"filetype" : "jpg",
+							"colour" : "red",
+							"license" : "labeled-for-noncommercial-reuse-with-modification",
+							"imageage" : "past-7-days",
+							"imagetype" : "photo",
+							"aspectratio" : "wide",
+							"imagesize" : "medium",
+							"serverhost" : "",
+							"serverusername" : "",
+							"serverpassword" : "",
+							"whitelist": ".edu",
+							"region" : "Greece",
+							"blacklist" : "wikipedia.ca"
+						}
+
+	def test_all_args(self):
+		sys.argv = ['Main.py', '-k', 'Default', '-l', '10', '-ss', '-d', '.\\Images', '-ft', 'jpg', '-c', 'red', '-li', 'labeled-for-noncommercial-reuse-with-modification', '-a', 'past-7-days', '-t', 'photo', '-ar', 'wide', '-is', 'medium', '-s', 'moore.mcmaster.ca', '-u', 'marin', '-p', 'no', '-wl', '.edu', '-rg', 'Greece', '-bl', 'wikipedia.ca']
+		args = userInput()
+
+		assert args == {
+							"keyword" : "Default",
+							"limit" : 10,
+							"safesearch" : True,
+							"directory" : ".\\Images",
+							"filetype" : "jpg",
+							"colour" : "red",
+							"license" : "labeled-for-noncommercial-reuse-with-modification",
+							"imageage" : "past-7-days",
+							"imagetype" : "photo",
+							"aspectratio" : "wide",
+							"imagesize" : "medium",
+							"serverhost" : "moore.mcmaster.ca",
+							"serverusername" : "marin",
+							"serverpassword" : "no",
+							"whitelist": ".edu",
+							"region" : "Greece",
+							"blacklist" : "wikipedia.ca"
+						}
+
 class TestKeywordFromFile:
 
 	def test_no_params(self):
